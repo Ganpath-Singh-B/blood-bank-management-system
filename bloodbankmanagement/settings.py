acbc891e3b73654b77d8ddb4f8536092a4bce64b
Ganpath-Singh-B <ganpathsingh288@gmail.com>
2024-05-17 17:14:36 +0530

    code updated

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,15 +83,15 @@ WSGI_APPLICATION = 'bloodbankmanagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
-DATABASES["default"]= dj_database_url.parse(os.getenv('DATABASE_URL'))
+# DATABASES["default"]= dj_database_url.parse(os.getenv('DATABASE_URL'))
 
 
 
